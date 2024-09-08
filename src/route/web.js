d1c80599d4641744af8366e40195af88e2a253c4
duckies <voducminh39@gmail.com>
2024-09-08 15:27:47 +0700

    db done@@

--- src/route/web.js
+++ src/route/web.js
@@ -3,8 +3,17 @@ import express from "express";
 let router = express.Router();
 
 let initWebRouter = (app) => {
+  /**
+   * @swagger
+   * /hi:
+   *   get:
+   *     summary: Get all users
+   *     responses:
+   *       200:
+   *         description: Success
+   */
   router.get("/hi", (req, res) => {
-    return res.status(200).json({ message: "Hello world" });
+    return res.status(200).json({ message: "Hello Rial" });
   });
   return app.use("/", router);
 };
--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import express from "express";
 import initWebRouter from "./route/web.js";
+import swaggerDocs from "./swagger.js";
 
-dotenv.config()
+dotenv.config();
 let app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,4 +11,5 @@ initWebRouter(app);
 let port = process.env.PORT || 8888;
 app.listen(port, () => {
   console.log(`Listening on port: http://localhost:${port}`);
+  swaggerDocs(app, port);
 });
